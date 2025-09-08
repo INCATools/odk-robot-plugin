da00d896ba1a2d6421db4d281377924709124a8f
Damien Goutte-Gattat <dpg44@cam.ac.uk>
2025-09-08 16:47:25 +0100

    Add a check for deprecated references.
    Add a new `check` command intended to perform some additional checks
    (beyond those already performed by ROBOT) on an ontology.
    
    For now, the only check is a check that no class and no object property
    is defined relatively to a deprecated entity.

---
+++ src/main/java/org/incenp/obofoundry/odk/CheckCommand.java
@@ -0,0 +1,120 @@
+/*
+ * ODK ROBOT Plugin
+ * Copyright © 2025 Damien Goutte-Gattat
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.incenp.obofoundry.odk;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.cli.CommandLine;
+import org.obolibrary.robot.CommandLineHelper;
+import org.obolibrary.robot.CommandState;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.parameters.Imports;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CheckCommand extends BasePlugin {
+
+    private static final Logger logger = LoggerFactory.getLogger(CheckCommand.class);
+
+    private Set<String> basePrefixes = new HashSet<>();
+
+    public CheckCommand() {
+        super("check", "perform some checks on an ontology", "robot check --checks [CHECK,...]");
+
+        options.addOption("c", "checks", true, "list of checks to perform");
+        options.addOption(null, "base-iri", true, "only check entities in the indicated namespace(s)");
+        options.addOption("x", "fail", true, "if true, fail if any of the checks fails");
+    }
+
+    @Override
+    public void performOperation(CommandState state, CommandLine line) throws Exception {
+        for ( String prefix : CommandLineHelper.getOptionalValues(line, "base-iri") ) {
+            basePrefixes.add(getIRI(prefix, "base-iri").toString());
+        }
+
+        int failed = 0;
+        for ( String check : line.getOptionValues("checks") ) {
+            switch ( check ) {
+            case "deprecated-references":
+                if ( !checkDeprecatedReferences(state.getOntology()) ) {
+                    logger.error("The ontology contains references to deprecated entities");
+                    failed += 1;
+                }
+                break;
+            }
+        }
+
+        if ( failed > 0 && CommandLineHelper.getBooleanValue(line, "fail", false) ) {
+            System.exit(1);
+        }
+    }
+
+    private boolean checkDeprecatedReferences(OWLOntology ontology) {
+        boolean pass = true;
+        for ( OWLEntity entity : ontology.getSignature(Imports.INCLUDED) ) {
+            if ( !isInBase(entity.getIRI().toString()) || isDeprecated(ontology, entity) ) {
+                continue;
+            }
+
+            Set<OWLAxiom> axioms = new HashSet<>();
+            if ( entity instanceof OWLClass ) {
+                axioms.addAll(ontology.getAxioms((OWLClass) entity, Imports.INCLUDED));
+            } else if ( entity instanceof OWLObjectProperty ) {
+                axioms.addAll(ontology.getAxioms((OWLObjectProperty) entity, Imports.INCLUDED));
+            }
+
+            for ( OWLAxiom axiom : axioms ) {
+                for ( OWLEntity referenced : axiom.getSignature() ) {
+                    if ( isDeprecated(ontology, referenced) ) {
+                        pass = false;
+                        logger.warn("{} references deprecated entity {}", entity.getIRI(), referenced.getIRI());
+                    }
+                }
+            }
+        }
+
+        return pass;
+    }
+
+    private boolean isDeprecated(OWLOntology ontology, OWLEntity entity) {
+        for ( OWLOntology ont : ontology.getImportsClosure() ) {
+            for ( OWLAnnotationAssertionAxiom ax : ont.getAnnotationAssertionAxioms(entity.getIRI()) ) {
+                if ( ax.isDeprecatedIRIAssertion() ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isInBase(String iri) {
+        for ( String base : basePrefixes ) {
+            if ( iri.startsWith(base) ) {
+                return true;
+            }
+        }
+        return basePrefixes.isEmpty();
+    }
+}
